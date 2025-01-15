996a1d0b15c483b98400b7c3d2f268f3d4da9bd7
theaavashh <aavash.ganeju@gmail.com>
2025-01-15 11:53:13 +0545

    chore: initial commit

---
+++ app/(pages)/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Layout = ({
+ children,
+}:Readonly<{children:React.ReactNode}>) => {
+  return (
+   <>
+   <div>Layout</div>
+   {children}
+   </>
+    
+  )
+}
+
+export default Layout
